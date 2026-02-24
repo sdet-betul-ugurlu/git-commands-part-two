e0d37d33174636145ca57654632c9e5922c3219e
Betül UĞURLU <sdet.betul.ugurlu@gmail.com>
2026-02-24 21:25:05 +0300

    Update TestCase06.java

--- src/tests/TestCase06.java
+++ src/tests/TestCase06.java
@@ -4,5 +4,13 @@ public class TestCase06 {
     public static void main(String[] args) {
         // Hello this is test case
         // Hello this is test case
+
+        // Hello this is test case
+        // Hello betul
+
+
+
+
+        
     }
 }
